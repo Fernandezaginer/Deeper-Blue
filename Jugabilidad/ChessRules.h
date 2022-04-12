420982621faa968e5ba10efe338eae382b77c957
Fernandezaginer <73385513+Fernandezaginer@users.noreply.github.com>
2022-04-12 20:24:36 +0200

    Update ChessRules.h

--- Jugabilidad/ChessRules.h
+++ Jugabilidad/ChessRules.h
@@ -19,6 +19,25 @@ using namespace std;
 // Puntero a las piezas de la partida
 typedef  pieza_t** tablero_t;
 
+// CRITERIO DEL TABLERO:
+
+/*
+T C A D R A C T  // Piezas negras
+P P P P P P P P
+- - - - - - - -
+- - - - - - - -
+- - - - - - - -
+P P P P P P P P
+T C A D R A C T  // Piezas blancas
+*/
+
+// Primera dimensión = Filas
+// Segunda dimensión = Columnas
+// EJ: El rey blanco está en [7][4]
+
+
+
+
 
 class Juego {
 private:
