eb68aa114d57dc254079838d9098e39a69ecd9de
Fernandezaginer <73385513+Fernandezaginer@users.noreply.github.com>
2022-04-14 19:49:58 +0200

    Update Pieza.h

--- Jugabilidad/Pieza.h
+++ Jugabilidad/Pieza.h
@@ -1,5 +1,8 @@
 
 
+#include <sstream>
+using namespace std;
+
 #ifndef _PIEZA_H_
 #define _PIEZA_H_
 
@@ -8,7 +11,7 @@
 #define NULL 0
 
 
-// Información de la pieza
+// InformaciÃ³n de la pieza
 enum forma_pieza_t { NO_PIEZA, PEON, CABALLO, ALFIL, TORRE, DAMA, REY };
 enum color_pieza_t { NO_COLOR, BLANCA, NEGRA };
 enum movimiento_t { PROPIA_PIEZA = -1, NO_PERMITIDO, PERMITIDO, COMER_PIEZA };
@@ -19,10 +22,17 @@ typedef struct {
 }tablero_info_t;
 
 
+string mov_print(tablero_info_t t); // Pruebas
+
 
 class pieza_t {
 	forma_pieza_t forma;
 	color_pieza_t color;
+
+	bool aux_detectar_comer_y_propias_piezas(tablero_info_t& matriz, int i, int j, pieza_t casilla, pieza_t* me);
+	void aux_permite_mov_vert_hor(tablero_info_t& matriz, pieza_t** tab, int row, int col, pieza_t* me);
+	void aux_permite_mov_diag(tablero_info_t& matriz, pieza_t** tab, int row, int col, pieza_t* me);
+
 public:
 
 	pieza_t() : forma(NO_PIEZA), color(NO_COLOR) {};
@@ -33,10 +43,8 @@ public:
 	void setForma(forma_pieza_t a) { forma = a; };
 	void setColor(color_pieza_t a) { color = a; };
 
-	tablero_info_t get_mov_permitidos(pieza_t a, pieza_t** tab);
+	tablero_info_t get_mov_permitidos(pieza_t** tab);
 };
-
-
 
 
 
