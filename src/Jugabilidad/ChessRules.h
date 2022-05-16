807e5296e4a4a7c9512e4a431b38beb388d076d5
Fernandezaginer <73385513+Fernandezaginer@users.noreply.github.com>
2022-05-16 07:17:15 +0200

    Update ChessRules.h

--- src/Jugabilidad/ChessRules.h
+++ src/Jugabilidad/ChessRules.h
@@ -1,13 +1,83 @@
+
+
 // Test unitario:
 /*
+
+#include <iostream>
+#include "chesstime.h";
+#include "ChessRules.h";
+
+
+#include <thread>
+#include <chrono>
+using std::this_thread::sleep_for;
+using namespace std::chrono_literals;
+
+
+int main(void)
+{
+
+	// Test unitario (ejemplo del mate del tonto)
 	Juego a(RAPIDA);
-	cout << a.print();
-	while (a.getMinLeftPlaA() == 10);
-	a.movimiento(6, 4, 4, 4);
-	cout << a.print();
-	while (a.getMinLeftPlaB() == 10);
-	a.movimiento(1, 4, 3, 4);
-	cout << a.print();
+
+	int secuencia = 0;
+	while (!a.jaque_mate_playerA() && !a.jaque_mate_playerB()) {
+
+		sleep_for(750ms);
+		secuencia++;
+
+		switch (secuencia)
+		{
+		case 1:
+			a.movimiento(6, 6, 4, 6);
+			//a.movimiento(static_cast<string>("Pg2 g4"));
+			cout << a.print();
+			break;
+		case 5:
+			a.movimiento(1, 4, 3, 4);
+			//a.movimiento(static_cast<string>("Pe7 e5"));
+			cout << a.print();
+			break;
+		case 10:
+			a.movimiento(6, 5, 5, 5);
+			//a.movimiento(static_cast<string>("Pf2 f3"));
+			cout << a.print();
+			break;
+		case 15:
+			a.movimiento(0, 3, 4, 7);
+			//a.movimiento(static_cast<string>("Pd8 h4"));
+			cout << a.print();
+			break;
+		default:
+			break;
+		}
+
+		// Print tiempo:
+		cout << a.getMinLeftPlaA() << ":" << a.getSecLeftPlaA() << " " << a.getMinLeftPlaB() << ":" << a.getSecLeftPlaB() << " " << a.jaque_mate_playerA() << std::endl;
+
+	}
+
+	// Fin de la  partida
+	cout << "Fin de la partida" << endl << endl;
+	cout << a.notacion_partida();
+
+
+
+	//tablero_t aux = a.getTablero();
+	//tablero_info_t info = a.mov_permitidos(&(aux[6][0]), aux);
+
+	//movimiento_t mov = info.TAB[5][0];
+	//mov = info.TAB[4][0];
+	//mov = info.TAB[4][5];
+
+	//cout << a.print();
+	//a.movimiento(6, 0, 5, 4);
+	//cout << a.print();
+
+	return 0;
+}
+
+
 */
 
 #include "chesstime.h"
@@ -23,16 +93,7 @@ using namespace std;
 // Puntero a las piezas de la partida
 typedef  pieza_t** tablero_t;
 
-enum casillas_t { a1 = 0, b1, c1, d1, e1, f1, g1, h1, a2, b2, c2, d2, e2, f2, g2, h2, a3, b3, c3, d3, e3, f3, g3, h3, a4, b4, c4, d4, e4, f4, g4, h4, a5, b5, c5, d5, e5, f5, g5, h5, a6, b6, c6, d6, e6, f6, g6, h6, a7, b7, c7, d7, e7, f7, g7, h7, a8, b8, c8, d8, e8, f8, g8, h8};
-struct movimiento {
-	movimiento_t mov;
-	int row_o;
-	int col_o;
-	int row_f;
-	int col_f;
-};
 
-typedef vector<movimiento> lista_movimientos;
 
 // CRITERIO DEL TABLERO:
 
@@ -65,9 +126,10 @@ public:
 
 	// Movimientos de las piezas:
 	tablero_info_t mov_permitidos(pieza_t* a, tablero_t tab);
-  lista_movimientos get_mov_permitidos_l(tablero_t tab, int row_o, int col_o);
 	bool movimiento(int row_o, int col_o, int row_f, int col_f);
-	ostream& notacion_ulimo_moviento();
+	bool movimiento(string mov);
+	string notacion_ulimo_moviento();
+	string notacion_partida();
 	tablero_t getTablero() { return tab; }
 	
 
@@ -84,9 +146,6 @@ public:
 
 	int score_playerA();
 	int score_playerB();
-  
-  tablero_t get_tablero() { return this->tab; }
-	void set_tablero(tablero_t tablero) { this->tab = tablero; }
 
 	// Servivios heredados de chestime.h:
 	/*
@@ -95,6 +154,7 @@ public:
 	int getSecLeftPlaB();
 	int getMinLeftPlaA();
 	int getMinLeftPlaB();
+
 	// Métodos para indicar si algún jugador pierde por tiempo
 	bool PlaA_end_time();
 	bool PlaB_end_time();
@@ -132,8 +192,11 @@ private:
 
 	// Notacion del movimietno
 	ostringstream mov;
+	ostringstream mov_partida;
 	char row_to_char(int row);
 	char col_to_char(int col);
+	int char_to_row(char row);
+	int char_to_col(char col);
 
 	// Reglas espieciales peón: comer al paso y promoción
  	void aux_detectar_comer_al_paso(tablero_info_t& matriz, pieza_t peon, int row, int col);
@@ -150,3 +213,4 @@ private:
 
 
 #endif
+
